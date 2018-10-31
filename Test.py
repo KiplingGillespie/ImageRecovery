d2b4659be84c4a218447ac8bfa71a90ad527e63e
Adam Brassfield <adam.brassfield@uky.edu>
2018-10-30 21:01:39 -0400

    Created Test.py, declared testing class for I/O

---
+++ Test.py
@@ -0,0 +1,11 @@
+# Author(s): Adam Brassfield
+# Date: October 30, 2018
+# File: Test.py
+# Purpose: Unit tests for Image Recovery application
+
+import unittest
+
+class TestIO(unittest.TestCase):
+
+if __name__ == "__main__":
+	unittest.main()
