74af3a78cfc0051e4e1b40222b69801d1c186881
Adam Brassfield <adam.brassfield@uky.edu>
2018-11-07 08:53:13 -0500

    Reorganized test modules into their own subdirectory, modified import statements to adjust for this change

---
+++ test/TestCommentCounter.py
@@ -0,0 +1,13 @@
+# Author(s): Adam Brassfield
+# Date: November 6, 2018
+# File: TestCommentCounter.py
+
+import unittest
+import sys
+sys.path.append("../")
+from CommentCounter import *
+
+
+class TestCommentCounter(unittest.TestCase):
+	def testGetComments():
+	def testGetWords():
